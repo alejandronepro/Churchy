efb07d37fb19a8af63bf7b4730951fb340a0edaa
Alejandro Valdez <alejandronepro@gmail.com>
2018-08-24 17:58:11 -0400

    Referenced bootstrap files.

--- index.php
+++ index.php
@@ -4,10 +4,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <link rel="stylesheet" href="../Churchy/bootstrap/css/bootstrap.css">
+    <link rel="stylesheet" href="../Churchy/css/index/index.css">
+
     <title>Church Website</title>
 </head>
 <body>
     <h1>Hello World!</h1>
     <p>Whats up.</p>
+
+    <script src="../Churchy/bootstrap/js/bootstrap.js"></script>
 </body>
 </html>
