8651c346dcfd4350ab830f2a726a3f8369820b9e
Alejandro Valdez <alejandronepro@gmail.com>
2018-08-24 22:32:29 -0400

    Creating a form

---
+++ login/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <link rel="stylesheet" href="/bootstrap/css/bootstrap.css">
+
+    <title>Login</title>
+</head>
+
+<body>
+    <form id="loginForm">
+        <div class="form-row">
+            <div class="col">
+                <input type="text" class="form-control" placeholder="Username" name="username" id="username">
+            </div>
+            <div class="col">
+                <input type="password" class="form-control" placeholder="Password" name="password" id="password">
+            </div>
+        </div>
+    </form>
+
+    <script src="/bootstrap/js/bootstrap.js"></script>
+</body>
+
+</html>
